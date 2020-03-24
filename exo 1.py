9d2b599346eaa330d26fc0e1efdf0c4e5312b24e
MathisDelonglee <mathis.delonglee@isen-ouest.yncrea.fr>
2020-03-24 11:23:13 +0100

    Initial commit

---
+++ exo 1.py
@@ -0,0 +1,33 @@
+class circle:
+    def __init__(self, rayon):
+        self.__rayon = rayon
+
+    def getRadius(self):
+        return self.__rayon
+
+    def __add__(self, c):
+       return circle(self.__rayon + c.__rayon)
+
+    def __gt__(self, c):
+       return self.__rayon < c.__rayon
+
+    def __it__(self, c):
+       return self.__rayon > c.__rayon
+
+
+
+if __name__=='__main__':
+    c1 = circle(2)
+    c2 = circle(3)
+    c3 = c1 + c2
+    c4 = c1 < c2
+    c5 = c2 > c3
+
+    print(c3.getRadius())
+    print("Instance :", isinstance(c3,circle))
+    print("**********")
+    print(c4)
+    print("Instance :", isinstance(c4,circle))
+    print("**********")
+    print(c5)
+    print("Instance :", isinstance(c5,circle))
